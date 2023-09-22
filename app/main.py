e343aab2dc6edc8f218b587404097668331bcb71
Sakine Busra Gocmez <62428632+sbgocmez@users.noreply.github.com>
2023-09-22 17:48:53 +0300

    crud, database

--- app/main.py
+++ app/main.py
@@ -4,6 +4,9 @@ from fastapi.params import Body
 from pydantic import BaseModel
 from typing import Optional
 from random import randrange
+import psycopg2
+from psycopg2.extras import RealDictCursor
+
 
 app = FastAPI()
 
@@ -13,6 +16,24 @@ class Post(BaseModel):
     content: str
     published: bool = True
     rating: Optional[int] = None
+    
+class Track(BaseModel):
+    device: str
+    platform: str
+    timestamp: str
+    language: str
+    
+
+# burayi while a almayi dusunebiliriz
+try:
+    conn = psycopg2.connect(host='localhost', database='fastapi', user='postgres', password='busra123', cursor_factory=RealDictCursor)
+    cursor = conn.cursor()
+    
+    print("Database connection is succesful!")
+except Exception as dbConnectionError:
+    print("Connecting to database was failed.")
+    print(dbConnectionError)
+    #time.sleep(3)
 
 
 my_posts = [{"title": "title of 1 ", "content":"content of p", "id":1}]
@@ -22,12 +43,46 @@ my_posts = [{"title": "title of 1 ", "content":"content of p", "id":1}]
 async def root():
     return {"message": "Hello World"}
 
-@app.get("/devices")
-def get_userDataDevice():
-    return {"device data": "Mobile"}
+@app.get("/tracks")
+def get_tracks():
+    cursor.execute("""SELECT * FROM public."Tracks" """)
+    tracks = cursor.fetchall()
+    print(tracks)
+    return {"device data": tracks}
+
+@app.post("/tracks", status_code=status.HTTP_201_CREATED)
+def create_tracks(track: Track):
+    variables = (track.device, track.platform, track.timestamp, track.language)
+    print(variables)
+    cursor.execute("""INSERT INTO public."Tracks" (device, platform, timestamp, language) VALUES (%s, %s, %s, %s) RETURNING *""", (track.device, track.platform, track.timestamp, track.language))
+    new_track = cursor.fetchone()
+    conn.commit()
+    return {"added": new_track}
+
+@app.get("/tracks/{id}")
+def get_one_track(id):
+    cursor.execute("""SELECt * FROM public."Tracks" where id = %s""", (id,))
+    track = cursor.fetchone()
+    
+    if not track:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="Not found id ")
+    
+    return(track)
+    
+
+@app.delete("/tracks/{id}", status_code=status.HTTP_204_NO_CONTENT)
+def delete_track(id:int):
+    cursor.execute("""DELETE FROM public."Tracks" WHERE id=%s returning *""", (str(id)))
+    deleted_track = cursor.fetchone()
+    conn.commit()
+    
+    if deleted_track == None:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="Not found id ")
+    return({"data ": "deleted_track"})
 
 @app.post("/devices")
 def send_userDataDevice(payLoad: dict = Body(...)):
+    
     print(payLoad['device'])
     return {
         "Send successfuly",
