044ea9cb7d4d2d2da0d805deb3f2ae1cc9cd69c6
Sakine Busra Gocmez <62428632+sbgocmez@users.noreply.github.com>
2023-09-22 13:30:14 +0300

    first get-posts

---
+++ main.py
@@ -0,0 +1,23 @@
+from fastapi import FastAPI
+from fastapi.params import Body
+
+app = FastAPI()
+
+# path operation
+# function and decorator
+@app.get("/")
+async def root():
+    return {"message": "Hello World"}
+
+@app.get("/devices")
+def get_userDataDevice():
+    return {"device data": "Mobile"}
+
+@app.post("/devices")
+def send_userDataDevice(payLoad: dict = Body(...)):
+    print(payLoad['device '])
+    return {
+        "status":"Send successfuly",
+        "device" : "Desktop",
+        "platform": "Windows"
+    }
