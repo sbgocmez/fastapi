7d7687fcb1eb3ab036bc4e925b19100778be995e
Sakine Busra Gocmez <62428632+sbgocmez@users.noreply.github.com>
2023-09-26 16:02:40 +0300

    dburl

--- app/database.py
+++ app/database.py
@@ -2,9 +2,11 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-SQLALCHEMY_DATABASE_URL = "postgresql://postgres:busra123@localhost/fastapi"
+# SQLALCHEMY_DATABASE_URL = "postgresql://postgres:busra123@localhost/fastapi"
 
-# SQLALCHEMY_DATABASE_URL = "postgres://wxdrjneihowrax:fd620896e4bec3696b5e48b8cd943f53d5374c553e176bf6ef1a68ceefeb62b1@ec2-52-205-171-232.compute-1.amazonaws.com:5432/d397m5f10p3vj7"
+# SQLALCHEMY_DATABASE_URL = "postgresql://wxdrjneihowrax:postgres:busra123@localhost/fastapi"
+
+SQLALCHEMY_DATABASE_URL = "postgresql://wxdrjneihowrax:fd620896e4bec3696b5e48b8cd943f53d5374c553e176bf6ef1a68ceefeb62b1@ec2-52-205-171-232.compute-1.amazonaws.com:5432/d397m5f10p3vj7"
 
 # 'postgresql://<username>:<password>@ip-address/hostname/<databasename>'
 
