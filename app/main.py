dea77f793c18fdbdb3cc2849f29ba9bc5b55f8b6
Sakine Busra Gocmez <62428632+sbgocmez@users.noreply.github.com>
2023-09-26 15:44:54 +0300

    gh

--- app/main.py
+++ app/main.py
@@ -17,10 +17,9 @@ visitModel.Base.metadata.create_all(bind=engine)
 
 app = FastAPI()
 
-origins= ["https://www.google.com"]
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=origins,
+    allow_origins=["*"],
     allow_credentials=True,
     allow_methods=["*"], # get post put ?
     allow_headers=["*"],
