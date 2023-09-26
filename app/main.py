e1d98c78ddc67eed0ece2eea8319f275923b6f87
Sakine Busra Gocmez <62428632+sbgocmez@users.noreply.github.com>
2023-09-26 13:52:52 +0300

    changes

--- app/main.py
+++ app/main.py
@@ -1,9 +1,20 @@
 from fastapi import FastAPI
-from .models import clientModel
+from .models import clientModel, visitModel
 from .database import engine
 from .routers import client
+from fastapi.middleware.cors import CORSMiddleware
 
 clientModel.Base.metadata.create_all(bind=engine)
+visitModel.Base.metadata.create_all(bind=engine)
 
 app = FastAPI()
+
+origins= ["https://www.google.com"]
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"], # get post put ?
+    allow_headers=["*"],
+)
 app.include_router(client.router)
---
+++ app/models/visitModel.py
@@ -0,0 +1,12 @@
+from ..database import Base
+from sqlalchemy import Column
+from sqlalchemy import Integer, ForeignKey
+from sqlalchemy.sql.sqltypes import TIMESTAMP
+from sqlalchemy.sql.expression import text
+
+class Visit(Base):
+    __tablename__ = "Visits"
+    id = Column(Integer, primary_key=True, nullable=False)
+    created_at = Column(TIMESTAMP(timezone=True))
+    closed_at = Column(TIMESTAMP(timezone=True))
+    client_id = Column(Integer, ForeignKey("Clients.id", ondelete="CASCADE"), nullable=False)
