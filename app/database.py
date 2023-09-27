5497c49420f98b13547235c0cb968045106fb804
Sakine Busra Gocmez <62428632+sbgocmez@users.noreply.github.com>
2023-09-27 17:16:07 +0300

    mysql db

--- app/database.py
+++ app/database.py
@@ -6,8 +6,19 @@ from sqlalchemy.orm import sessionmaker
 
 # SQLALCHEMY_DATABASE_URL = "postgresql://wxdrjneihowrax:postgres:busra123@localhost/fastapi"
 
-SQLALCHEMY_DATABASE_URL = "postgresql://wxdrjneihowrax:fd620896e4bec3696b5e48b8cd943f53d5374c553e176bf6ef1a68ceefeb62b1@ec2-52-205-171-232.compute-1.amazonaws.com:5432/d397m5f10p3vj7"
+# SQLALCHEMY_DATABASE_URL = "postgresql://wxdrjneihowrax:fd620896e4bec3696b5e48b8cd943f53d5374c553e176bf6ef1a68ceefeb62b1@ec2-52-205-171-232.compute-1.amazonaws.com:5432/d397m5f10p3vj7"
 
+
+db_endpoint = "mydb-api-mysql.cibbmu3dasbu.eu-north-1.rds.amazonaws.com"
+db_port = 3306  # Default MySQL port
+db_username = "admin"
+db_name = "mydatabase"
+db_password = "busra123"
+
+# Create the SQLAlchemy database URL
+db_url = f"mysql+pymysql://{db_username}:{db_password}@{db_endpoint}:{db_port}/{db_name}"
+
+SQLALCHEMY_DATABASE_URL = db_url
 # 'postgresql://<username>:<password>@ip-address/hostname/<databasename>'
 
 engine = create_engine(SQLALCHEMY_DATABASE_URL)
--- app/models/clientModel.py
+++ app/models/clientModel.py
@@ -7,8 +7,8 @@ from sqlalchemy.sql.expression import text
 class Client(Base):
     __tablename__ = "Clients"
     id = Column(Integer, primary_key=True, nullable=False)
-    address = Column(String, nullable=False)
-    device = Column(String, nullable=False)
-    platform = Column(String, nullable=False)
-    language = Column(String, nullable=False)
+    address = Column(String(64), nullable=False)
+    device = Column(String(32), nullable=False)
+    platform = Column(String(32), nullable=False)
+    language = Column(String(32), nullable=False)
     created_at = Column(TIMESTAMP(timezone=True), nullable = False, server_default=text('now()'))
