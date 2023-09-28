c1da3b6d6e3e447bb379b3eb94bc71c98253f19a
Sakine Busra Gocmez <62428632+sbgocmez@users.noreply.github.com>
2023-09-28 10:44:19 +0300

    local

--- app/database.py
+++ app/database.py
@@ -3,12 +3,12 @@ from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 #import pymysql
 
-# SQLALCHEMY_DATABASE_URL = "postgresql://postgres:busra123@localhost/fastapi"
+SQLALCHEMY_DATABASE_URL = "postgresql://postgres:busra123@localhost/fastapi"
 
 # SQLALCHEMY_DATABASE_URL = "postgresql://wxdrjneihowrax:postgres:busra123@localhost/fastapi"
 
 # heroku
-SQLALCHEMY_DATABASE_URL = "postgresql://wxdrjneihowrax:fd620896e4bec3696b5e48b8cd943f53d5374c553e176bf6ef1a68ceefeb62b1@ec2-52-205-171-232.compute-1.amazonaws.com:5432/d397m5f10p3vj7"
+#SQLALCHEMY_DATABASE_URL = "postgresql://wxdrjneihowrax:fd620896e4bec3696b5e48b8cd943f53d5374c553e176bf6ef1a68ceefeb62b1@ec2-52-205-171-232.compute-1.amazonaws.com:5432/d397m5f10p3vj7"
 
 ## AWS
 # db_endpoint = "mydb-api-mysql.cibbmu3dasbu.eu-north-1.rds.amazonaws.com"
